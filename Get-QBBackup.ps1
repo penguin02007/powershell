7d9f5d86b797fabcd87fa5df89da1989529f6407
Leo Chan <lchan@HSTF.local>
2018-03-06 00:32:52 -0500

    add parameter and include detail message

--- Get-QBBackup.ps1
+++ Get-QBBackup.ps1
@@ -1,27 +1,33 @@
 function Get-QBBackup {
-  $QBBPath = 'C:\options\qb2017'
-  $QBBFiles = Get-ChildItem $QBBPath\*QBB
+  $QBCwd = 'C:\options\qb\cwd'
+  $QBBackUpPath = 'C:\options\qb2017'
+  $QBBFiles = Get-ChildItem $QBBackUpPath\*QBB
   $NumOfQBBFiles = $QBBFiles.Count
+  $NameOfQBFiles = $QBBFiles.Name -join "`n"
   $NumOfQBBSinceYday = ($QBBFiles | Where {$_.LastWriteTime -ge (Get-Date).AddDays(-1)}).count
   if( $QBBFiles.LastWriteTime -ge [datetime]::Today){ 
-     $BackupMessage = "## Backup was ran successfully in $QBBPath. ##"
+     $BackupMessage = "SUCCESS :: Backup ran successfully."
   }else{
-     $BackupMessage = "## Backup was not run or there is error encountered. Check in $QBBPath. ##"
+     $BackupMessage = "WARNING :: Backup did not run."
   }
   $FullBackMessage ="`
-  $BackupMessage `
-  There are $NumOfQBBFiles total backup(s) and $NumOfQBBSinceYday backup(s) ran today.`
-  "
+####################################################################################`
+$BackupMessage `
+####################################################################################`
+Working Directory - $QBCwd
+Backup Directory - $QBBackUpPath
+There are $NumOfQBBFiles backup(s) and $NumOfQBBSinceYday backup(s) ran today.`
+$NameOfQBFiles
+`n`
+"
   return $FullBackMessage
 }
-
 function Send-HSMail {
-  $to = 'finance@hydesquare.org'
-  $bcc = 'leochan@hydesquare.org'
+  param( [string]$to,[string]$bcc )
   $from = 'quickbooks@hydesquare.org'
   $subject = 'Quickbooks Backup Status'
   $smtpserver = 'smtp-relay.gmail.com'
   $body = Get-QBBackup
   Send-MailMessage -To $to -Bcc $bcc -From $from -Subject $subject -SmtpServer $smtpserver -UseSsl -Body $body
 }
-Send-HSMail
+Send-HSMail -to leochan@hydesquare.org -bcc leochan@hydesquare.org
