2edbcc4e1566ad6df6247f9fdf730368aef61023
Leo Chan <penguin02007@gmail.com>
2018-11-26 09:58:08 -0500

    scripts for Dell Update Packages silent install

---
+++ dup_silent_installer/install_pkg.ps1
@@ -0,0 +1,42 @@
+write-host Installing 7389_Realtek-USB-Memory-Card-Reader-Driver_3VWV3_WIN_10.0.15063.31236_A00.EXE
+.\packages\7389_Realtek-USB-Memory-Card-Reader-Driver_3VWV3_WIN_10.0.15063.31236_A00.EXE /s | out-null
+write-host Installing ASMedia-USB-Extended-Host-Controller-Driver_JCDN0_WIN_1.16.54.1_A10.EXE
+.\packages\ASMedia-USB-Extended-Host-Controller-Driver_JCDN0_WIN_1.16.54.1_A10.EXE /s | out-null
+write-host Installing Dell-Command-Update_DDVDP_WIN_2.4.0_A00.EXE
+.\packages\Dell-Command-Update_DDVDP_WIN_2.4.0_A00.EXE /s | out-null
+write-host Installing Dell-Digital-Delivery-Application_8C27J_WIN_3.5.2006.0_A08.EXE
+.\packages\Dell-Digital-Delivery-Application_8C27J_WIN_3.5.2006.0_A08.EXE /s | out-null
+write-host Installing Dell-Touchpad-Driver_432G0_WIN_10.3201.101.209_A03.EXE
+.\packages\Dell-Touchpad-Driver_432G0_WIN_10.3201.101.209_A03.EXE /s | out-null
+write-host Installing Docks_Stands_Driver_G8VCP_WN32_2.44.2018.0504_A10.EXE
+.\packages\Docks_Stands_Driver_G8VCP_WN32_2.44.2018.0504_A10.EXE /s | out-null
+write-host Installing Intel-9x60-826x-726x-3165-Bluetooth-UWD-Driver_1CC7N_WIN_20.60.1_A22.EXE
+.\packages\Intel-9x60-826x-726x-3165-Bluetooth-UWD-Driver_1CC7N_WIN_20.60.1_A22.EXE /s | out-null
+write-host Installing Intel-Chipset-Device-Software-Driver_H41NR_WIN_10.1.1.44_A07_03.EXE
+.\packages\Intel-Chipset-Device-Software-Driver_H41NR_WIN_10.1.1.44_A07_03.EXE /s | out-null
+write-host Installing Intel-Dynamic-Platform-and-Thermal-Framework_D4G6R_WIN_8.3.10207.5567_A09.EXE
+.\packages\Intel-Dynamic-Platform-and-Thermal-Framework_D4G6R_WIN_8.3.10207.5567_A09.EXE /s | out-null
+write-host Installing Intel-HD-Graphics-500-P500-600-P600-series-Driver_DHV1V_WIN_24.20.100.6286_A10.EXE
+.\packages\Intel-HD-Graphics-500-P500-600-P600-series-Driver_DHV1V_WIN_24.20.100.6286_A10.EXE /s | out-null
+write-host Installing Intel-Integrated-Sensor-Solution-Driver_7TH4D_WIN_3.10.100.3429_A00_04.EXE
+.\packages\Intel-Integrated-Sensor-Solution-Driver_7TH4D_WIN_3.10.100.3429_A00_04.EXE /s | out-null
+write-host Installing Intel-Management-Engine-Components-Installer_JJMT9_WIN_1829.12.0.1154_A02.EXE
+.\packages\Intel-Management-Engine-Components-Installer_JJMT9_WIN_1829.12.0.1154_A02.EXE /s | out-null
+write-host Installing Intel-Rapid-Storage-Technology-Driver-and-Management_5P0PK_WIN_15.7.6.1027_A04.EXE
+.\packages\Intel-Rapid-Storage-Technology-Driver-and-Management_5P0PK_WIN_15.7.6.1027_A04.EXE /s | out-null
+write-host Installing Intel-Serial-IO-Driver_4T04G_WIN_30.100.1725.1_A04.EXE
+.\packages\Intel-Serial-IO-Driver_4T04G_WIN_30.100.1725.1_A04.EXE /s | out-null
+write-host Installing Intel-Thunderbolt-Controller-Driver_886C9_WIN_17.4.77.400_A10.EXE
+.\packages\Intel-Thunderbolt-Controller-Driver_886C9_WIN_17.4.77.400_A10.EXE /s | out-null
+write-host Installing Intel-Virtual-Buttons-Driver_MCFJ3_WIN_1.1.1.22_A02.EXE
+.\packages\Intel-Virtual-Buttons-Driver_MCFJ3_WIN_1.1.1.22_A02.EXE /s | out-null
+write-host Installing Network_Driver_1FG2H_WN32_20.50.0.8334_A14_02.EXE
+.\packages\Network_Driver_1FG2H_WN32_20.50.0.8334_A14_02.EXE /s | out-null
+write-host Installing Realtek-High-Definition-Audio-Driver_88TTT_WIN_6.0.1.8432_A16_01.EXE
+.\packages\Realtek-High-Definition-Audio-Driver_88TTT_WIN_6.0.1.8432_A16_01.EXE /s | out-null
+write-host Installing Realtek-USB-Audio-Driver_CCV58_WIN_6.3.9600.172_A09.EXE
+.\packages\Realtek-USB-Audio-Driver_CCV58_WIN_6.3.9600.172_A09.EXE /s | out-null
+write-host Installing SupportAssistLauncher.exe
+.\packages\SupportAssistLauncher.exe /s | out-null
+write-host Installing _Intel-HID-Event-Filter-Driver_9599G_WIN_2.2.1.372_A09.EXE
+.\packages\_Intel-HID-Event-Filter-Driver_9599G_WIN_2.2.1.372_A09.EXE /s | out-null
---
+++ dup_silent_installer/prepare.ps1
@@ -0,0 +1,15 @@
+﻿# Dell Update Packages Silent Installer
+# 
+# Prepare.ps1 - Prepare install script
+# install_pkg.ps1 - Install Dell Update Packages 
+#
+$pkgs           = Get-ChildItem packages
+$install_script = 'install_pkg.ps1'
+if (test-path $install_script -PathType Leaf) {
+  del $install_script
+}
+foreach ($pkg in $pkgs) {
+  Write-Output "write-host Installing $pkg" | Add-Content $install_script
+  Write-Output ".\packages\$pkg /s | out-null" | Add-Content $install_script
+}
+Get-Content $install_script
