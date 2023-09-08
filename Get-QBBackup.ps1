9546c4de0f16880370232bb7919014c65f3eba9a
Leo Chan <penguin02007@users.noreply.github.com>
2023-09-08 00:19:33 -0400

    Update Get-QBBackup.ps1
    Change max age days
    update mail subject to indicate success or warning

--- Get-QBBackup.ps1
+++ Get-QBBackup.ps1
@@ -2,7 +2,7 @@ $QBCwd = 'C:\options\qb\cwd'
 $QBBackUpPath = '\\hstf.local\public\aaOfficeMgr\qb'
 $QBBFiles = Get-ChildItem $QBBackUpPath\*QBB
 $Today = (Get-Date).Date
-$MaxAgeInDays = 14
+$MaxAgeInDays = 30
 
 function Purge-QBBackup {
   foreach ($file in $QBBFiles) {
@@ -13,35 +13,38 @@ function Purge-QBBackup {
 }
 
 function Get-QBBackup {
-  $QBBFiles = Get-ChildItem $QBBackUpPath\*QBB
+  $QBBFiles = Get-ChildItem "$QBBackUpPath\*Hyde Square Task Force*QBB"
   $NumOfQBBFiles = $QBBFiles.Count
   $NameOfQBFiles = $QBBFiles.Name -join "`n"
   $NumOfQBBSinceYday = ($QBBFiles | Where {$_.LastWriteTime -ge (Get-Date).AddDays(-1)}).count
-  if( $QBBFiles.LastWriteTime -ge [datetime]::Today){ 
+  if( $QBBFiles.LastWriteTime -ge [datetime]::Today){
      $BackupMessage = "SUCCESS :: Backup ran successfully today."
+     $Subject       = "Quickbooks Backup Status - Success" 
   }else{
      $BackupMessage = "WARNING :: Backup did not run today."
+     $Subject       = "Quickbooks Backup Status - Warning" 
   }
   $FullBackMessage ="`
-####################################################################################`
+##########################################################################`
 $BackupMessage `
-####################################################################################`
+##########################################################################`
 Working Directory - $QBCwd
 Backup Directory - $QBBackUpPath
 There are $NumOfQBBFiles backup(s) and $NumOfQBBSinceYday backup(s) ran today.`
 $NameOfQBFiles
 `n`
 "
-  return $FullBackMessage
+  $Result = @($Subject, $FullBackMessage)
+  return $Result
 }
 
 function Send-QBReport {
   param( [string]$to,[string]$bcc )
-  $from = 'quickbooks@hydesquare.org'
-  $subject = 'Quickbooks Backup Status'
-  $smtpserver = 'smtp-relay.gmail.com'
-  $body = Get-QBBackup
+  $from          = 'quickbooks@hydesquare.org'
+  $smtpserver    = 'smtp-relay.gmail.com'
+  $Result        = Get-QBBackup
+  $Subject       = $Result[0]
+  $body          = $Result[1]
   Send-MailMessage -To $to -Bcc $bcc -From $from -Subject $subject -SmtpServer $smtpserver -UseSsl -Body $body
 }
- 
- Purge-QBBackup
+Purge-QBBackup
