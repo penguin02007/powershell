9cd7822d2810af55da2f6a0476e89bc112646f10
Leo <penguin02007@gmail.com>
2025-05-10 15:10:47 -0400

    Refactor hstf-tools.ps1: streamline functions and enhance NVR hostname check

--- hstf-tools.ps1
+++ hstf-tools.ps1
@@ -1,87 +1,42 @@
-#  Must be running as administrator
-
-$lab_pcs = 'lab209,lab210,lab211,lab212,lab213,lab214,lab215,lab207'.Split(',')
-
-function testif-admin {
-  $user = [Security.Principal.WindowsIdentity]::GetCurrent();
-(New-Object Security.Principal.WindowsPrincipal $user).IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
-}
-
-function test-web-connection {
-  if (-not(testif-admin)) {
-    write-host "Must be running as administrator."
-    break
-  }
-  $lab_pcs | %{
-    $ErrorActionPreference = 'Stop'
-    $pc  = $_
-    $src = 'http://www.msn.com'
-    $dst = 'c:\test-web-conn.log'
-    $wc  = New-Object System.Net.WebClient
-    try {
-      Test-Connection $pc -count 1| Out-Null
-      $wc.DownloadFile($src,$dst)
-      remove-item $dst
-      Write-Host "$pc - succeeded"
-    }
-    catch {
-      Write-host "$pc - fail"
-    }
-  }
-}
-
-function shutdown-lab {
-  if (-not(testif-admin)) {
-    write-host "Must be running as administrator."
-    break
-  }
-  $lab_pcs | % {
-    $ErrorActionPreference = 'stop'
-    try {
-    $pc  = $_
-    stop-computer -comp $pc -force
-    write-host "$pc - shutdown success"
-    }
-    catch {
-    write-host "$pc - shutdown failed"
-    }
-  }
-}
-
-function restart-lab {
-  $lab_pcs | % {
-    $ErrorActionPreference = 'stop'
-    try {
-    $pc  = $_
-    restart-computer -comp $pc -force
-    write-host "$pc - shutdown success"
-    }
-    catch {
-    write-host "$pc - shutdown failed"
-    }
-  }
-}
+#  Run as Admin
 
 function install-choco {
-  Set-ExecutionPolicy Bypass -Scope Process -Force;
   iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 }
 
-function install-common-apps {
-  choco install vcredist140 vcredist2015 sharex git adobereader vlc jre8 putty strawberryperl firefox googlechrome zoom dotnet4.5 python -y
-}
-
 function install-nvr-apps {
-  choco install tightvnc
-}
+  $hostname       = $env:COMPUTERNAME
+  $nvrWorkStation = "nvr"
+  if ($hostname -match $nvrWorkStation) {
+    Write-Host "Hostname matches NVR."
+    choco install tightvnc -y
+  } else {
+    Write-Host "Hostname does not match NVR."
+  }
 
 function install-imageglass {
  choco install imageglass -y
- $extension = ".heic"
- $imageGlassPath = "C:\Program Files\ImageGlass\ImageGlass.exe"  # Update this path if your ImageGlass is installed elsewhere
- $progId = "ImageGlass.heic"
+ $extension      = ".heic"
+ $imageGlassPath = "C:\Program Files\ImageGlass\ImageGlass.exe"
+ $progId         = "ImageGlass.heic"
 
   # Register the file extension with a ProgID
   New-Item -Path "HKCU:\Software\Classes\$extension" -Force | Out-Null
   Set-ItemProperty -Path "HKCU:\Software\Classes\$extension" -Name "(Default)" -Value $progId
+}
+
+function install-common-apps {
+  choco install vcredist140 vcredist2015 sharex git adobereader vlc jre8 putty firefox googlechrome zoom dotnet4.5 python -y
+}
+
+function install_all {
+  [CmdletBinding(SupportsShouldProcess=$true,
+  ConfirmImpact='Medium')]
+  Set-ExecutionPolicy Bypass -Scope Process -Force;
+
+  install-choco
+  install-imageglass
+  install-common-apps
+  install-nvr-apps
+  Write-Host "All applications installed successfully."
 }
